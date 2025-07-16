e6b3d01914553feaf1a390c00e6f867850e8b029
hackerjack9 <lucekl03@frontier.com>
2025-07-16 11:20:50 -0400

    Highlights

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Nav from './components/Nav';
 import Landing from './components/Landing';
+import Highlights from './components/Highlights';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Nav />
       <Landing />
+      <Highlights />
    </div>
    );
 }
