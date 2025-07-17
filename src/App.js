705867351dbc53a63cc26dcd44ad3a6bc210c835
hackerjack9 <lucekl03@frontier.com>
2025-07-16 23:06:18 -0400

    Books

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import Nav from './components/Nav';
-import Landing from './components/Landing';
-import Highlights from './components/Highlights';
-
+import "./App.css";
+import Footer from './components/Footer';
+import {BrowerRouter as Router, Route} from '.react-router-dom';
+import Home from "./pages/Home";
+import Books from './pages/Books';
+import { books } from "../data";
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Nav />
-      <Landing />
-      <Highlights />
+      <Route path='/' exact componet={Home}/>
+      <Route path='/books' render={() => <Books books={books} />} />
+      <Footer />
    </div>
+   </Router>
    );
 }
 
